eeec10981c35937ca224158178fce107ae94131d
KlassyNihal <harshaan.n.khan@gmail.com>
2019-05-17 19:09:55 +0530

    before pull

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/dashboard', function (req, res, next) {
+  res.render('dashboard', { title: 'Express' });
+});
+
 
 module.exports = router;
