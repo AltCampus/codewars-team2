217489fe9876e8a35705ce59d74cce22ec373575
sajan <sajanloveu123@gmail.com>
2019-05-17 16:27:11 +0530

    update 2

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+mongoose.connect("mongodb://localhost/bookList", { useNewUrlParser: true }, (err) => {
+	err ? console.log(err, 'not connected to mongodb') : console.log('Successfully connected to mongodb');
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
