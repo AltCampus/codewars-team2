9ab6d07e74836ce21e54e0c2ec1a656d31eea268
SG24 <shreyanshgupta98@gmail.com>
2019-05-18 12:30:29 +0530

    added homePAge and dashboard

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var authController = require('../controller/authController');
 
 /* GET home page. */
 router.get('/', authController.isUserLoggedIn, (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AltWars' });
 });
 
 // rendering dashboard
